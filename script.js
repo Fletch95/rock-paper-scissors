cb3fccdca6dd53fd2287d9ecd8846c1f5b2fa0ac
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-08-05 11:30:08 +0100

    edit getComputerChoice to return text choice

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getComputerChoice() {
-    let rnd = Math.floor(Math.random()*3)
-    console.log(rnd)
+    let rnd = Math.floor(Math.random()*3);
+    console.log(rnd);
+
+    switch (rnd) {
+        case 0:
+            return "Rock";
+
+        case 1:
+            return "Paper";
+
+        case 2:
+            return "Scissors";
+    }
 }
